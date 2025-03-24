bf538b3d3143e8b1e8ea8ec502431f2c68c7eeca
MUKILTEO\1721887 <jbkjellin07@gmail.com>
2025-03-24 11:29:11 -0700

    <Working on the night vision mask's purple filter>

--- Q4/Assets/Josiah/Scripts/Mask.cs
+++ Q4/Assets/Josiah/Scripts/Mask.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-public class Mask
+public abstract class Mask : MonoBehaviour
 {
+    public KeyCode key;
     public string maskName;
 
-    public Mask()
+    private bool isMaskOn;
+
+    public abstract void EquipMask();
+    public abstract void UnequipMask();
+
+    public void Update()
     {
-        maskName = "Night vision mask";
+        if (Input.GetKeyDown(key))
+        {
+            if (isMaskOn) 
+            {
+                UnequipMask();
+            }
+            else
+            {
+                EquipMask();
+            }
+
+            // Flips bool to the opposite of what it was
+            isMaskOn = !isMaskOn;
+        }
     }
 }
--- Q4/Assets/Josiah/Scripts/NightvisionMask.cs
+++ Q4/Assets/Josiah/Scripts/NightvisionMask.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class NightvisionMask : MonoBehaviour
+public class NightvisionMask : Mask
 {
     [SerializeField] private Camera cam;
     [SerializeField] private LayerMask maskOff;
     [SerializeField] private LayerMask maskOn;
+    [SerializeField] private Color maskColor;
 
-
-    public void Start()
+    public override void EquipMask()
     {
         cam.cullingMask = maskOn;
+
+        var volume = cam.GetComponent<Volume>().sharedProfile;
+
+        if (volume.TryGet(out ColorAdjustments color))
+        {
+            color.colorFilter.Override(maskColor);
+        }
+    }
+
+    public override void UnequipMask()
+    {
+        cam.cullingMask = maskOff;
+
+        var volume = cam.GetComponent<Volume>().sharedProfile;
+
+        if (volume.TryGet(out ColorAdjustments color))
+        {
+            color.colorFilter.Release();
+        }
     }
 
 }
