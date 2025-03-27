c659ec12a07b3eaf4eda4b25a84a667a2acec06f
MUKILTEO\1721887 <jbkjellin07@gmail.com>
2025-03-27 13:29:20 -0700

    Commented code. Began making levers

---
+++ Q4/Assets/Josiah/Scripts/Lever.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Lever : MonoBehaviour
+{
+    public string message;
+
+    public UnityEvent onInteraction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    
+}
--- Q4/Assets/Josiah/Scripts/MoonMask.cs
+++ Q4/Assets/Josiah/Scripts/MoonMask.cs
@@ -15,26 +15,35 @@ public class MoonMask : Mask
     {
         cam.cullingMask = maskOn;
 
+        // Takes camera's volume profile in order to make changes to it
         var volume = cam.GetComponent<Volume>().profile;
 
+        // TryGet accesses component if the object it's attached to has that component. Returns a true bool if it has it, returns a false bool if it doesn't
         if (volume.TryGet(out ColorAdjustments color)&& volume.TryGet(out ChromaticAberration chromaticAberration))
         {
+            // Starts FadeIn coroutine when you equip the mask. Passes in color filter and chromatic aberration in order to change them
             StartCoroutine(FadeIn(color, chromaticAberration));
         }
     }
 
+    // All coroutines start with IEnumerator. Runs code when you want it instead of the way unity wants it. Allows you to control timing of code
     private IEnumerator FadeIn(ColorAdjustments color, ChromaticAberration chromaticAberration)
     {
+        // Makes code happen 20 times
         for (int i = 0; i < 20; i++)
         {
+            // Lerps value between white and the mask color over time
             color.colorFilter.Override(Color.Lerp(Color.white, maskColor, i / 20.0f));
+            // Lerps the intensity between 0 and 5
             chromaticAberration.intensity.Override(Mathf.Lerp(0, 0.5f, i / 20.0f));
+            // Forces the code to happen over time
             yield return new WaitForSeconds(0.025f);
         }
     }
 
     public override void UnequipMask()
     {
+        // Turns off the "Invisible" culling mask so you can't see it
         cam.cullingMask = maskOff;
 
         var volume = cam.GetComponent<Volume>().profile;
@@ -46,6 +55,7 @@ public class MoonMask : Mask
         }
     }
 
+    // Does the opposite of the other coroutine since the numbers are flipped
     private IEnumerator FadeOut(ColorAdjustments color, ChromaticAberration chromaticAberration)
     {
         for (int i = 0; i < 20; i++)
