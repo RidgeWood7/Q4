a2a648f7bb0f3c614bfc49cea0bf23bce3214460
MUKILTEO\1721887 <jbkjellin07@gmail.com>
2025-03-28 12:07:12 -0700

    <Started making the lever script/basic interaction script>

---
+++ Q4/Assets/Josiah/Scripts/Interactable.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public string message;
+
+    public UnityEvent onInteraction;
+
+    public float playerReach = 3f;
+    Interactable currentInteractable;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteraction();
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        //if colliders with anything within player reach
+        if (Physics.Raycast(ray, out hit, playerReach))
+        {
+            if (hit.collider.CompareTag("Interactable")) //if looking at an interactable object
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+
+                if (newInteractable.enabled)
+                {
+                    SetNewCurrentInteractable(newInteractable);
+                }
+            }
+
+            else //if not interactable
+            {
+                DisableCurrentInteractable();
+            }
+        }
+
+        else //if not within reach
+        {
+            DisableCurrentInteractable();
+        }
+    }
+
+    public void Interact()
+    {
+        onInteraction.Invoke();
+
+    }
+
+    void SetNewCurrentInteractable(Interactable newInteractable)
+    {
+        currentInteractable = newInteractable;
+    }
+
+    void DisableCurrentInteractable()
+    {
+        if (currentInteractable)
+        {
+            currentInteractable = null;
+        }
+    }
+}
